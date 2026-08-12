98515e6d89a3afe251b2a2b18d4662caa9ef520b
juanest2025 <velezhincapiejuan@gmail.com>
2026-08-12 17:59:29 -0500

    up

--- fuzzel/sitios/udea.sh
+++ fuzzel/sitios/udea.sh
@@ -5,6 +5,8 @@ declare -A sites=(
     ["Fisica Mecanica"]="https://udearroba.udea.edu.co/internos/course/view.php?id=26186"
     ["Ingles V"]="https://virtualingenieriaudea.co/course/view.php?id=4969"
     ["Algebra lineal"]="https://udearroba.udea.edu.co/internos/course/view.php?id=27000"
+    ["Classroom Fisica"]="https://classroom.google.com/c/ODc0MzgwMTMwMTMy"
+    ["Drive Ingles"]="https://udeaeduco-my.sharepoint.com/shared?listurl=https%3A%2F%2Fudeaeduco%2Dmy%2Esharepoint%2Ecom%2Fpersonal%2Faracelly%5Fospina%5Fudea%5Fedu%5Fco%2FDocuments&e=5%3Abb3aac9a086840b9859382a8c185882f&sharingv2=true&fromShare=true&at=9&id=%2Fpersonal%2Faracelly%5Fospina%5Fudea%5Fedu%5Fco%2FDocuments%2F2026%2D2%20EforE%20students%2FEnglish%20V%2FGroup%2018%20%2D%202026%2D2&FolderCTID=0x012000A5F08FC96EEC3B468C71B876883C6992"
 )
 
 selection=$(printf "%s\n" "${!sites[@]}" | sort | fuzzel --dmenu --prompt="Materias: ")
