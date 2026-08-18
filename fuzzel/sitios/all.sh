aa516eff150217687807c23dc61b3041f7255193
juanest2025 <velezhincapiejuan@gmail.com>
2026-08-18 12:36:26 -0500

    up

---
+++ fuzzel/sitios/all.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+google-chrome-stable \
+    "https://mail.google.com/mail/u/0/#inbox" \
+    "https://udearroba.udea.edu.co/internos/course/view.php?id=27000&section=0#tabs-tree-start" \
+    "https://udearroba.udea.edu.co/internos/course/view.php?id=26948&section=0#tabs-tree-start" \
+    "https://udearroba.udea.edu.co/internos/course/view.php?id=26186&section=0#menu-tree-start" \
+    "https://classroom.google.com/c/ODc0MzgwMTMwMTMy" \
+    "https://virtualingenieriaudea.co/course/view.php?id=4969" \
+    "https://udeaeduco-my.sharepoint.com/shared?id=%2Fpersonal%2Faracelly%5Fospina%5Fudea%5Fedu%5Fco%2FDocuments%2F2026%2D2%20EforE%20students%2FEnglish%20V%2FGroup%2018%20%2D%202026%2D2%2FUnit%201%20%2D%20Modules%20and%20Audios&listurl=%2Fpersonal%2Faracelly%5Fospina%5Fudea%5Fedu%5Fco%2FDocuments&viewid=0a05c85d%2D8d6b%2D469e%2Daefa%2D33b93e49ce4e&sharingv2=true&fromShare=true&at=9&FolderCTID=0x012000A5F08FC96EEC3B468C71B876883C6992" \
