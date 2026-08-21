bfd5c2c9303e21a96e9dcb821cd235a6c5b17a7f
juanest2025 <velezhincapiejuan@gmail.com>
2026-08-21 18:36:58 -0500

    up

--- fuzzel/sitios/udea.sh
+++ fuzzel/sitios/udea.sh
@@ -2,6 +2,7 @@
 
 declare -A sites=(
     ["Ude@"]="https://udearroba.udea.edu.co/internos/my/"
+    ["Ingeni@"]="https://virtualingenieriaudea.co/my/"
     ["Calculo integral"]="https://udearroba.udea.edu.co/internos/course/view.php?id=26948"
     ["Fisica Mecanica"]="https://udearroba.udea.edu.co/internos/course/view.php?id=26186"
     ["Ingles V"]="https://virtualingenieriaudea.co/course/view.php?id=4969"
