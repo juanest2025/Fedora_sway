93744fd5797ad22bae5ab6488632f8b8e6211cd4
juanest2025 <velezhincapiejuan@gmail.com>
2026-08-26 17:54:34 -0500

    up

--- fuzzel/sitios/udea.sh
+++ fuzzel/sitios/udea.sh
@@ -8,7 +8,8 @@ declare -A sites=(
     ["Ingles V"]="https://virtualingenieriaudea.co/course/view.php?id=4969"
     ["Algebra lineal"]="https://udearroba.udea.edu.co/internos/course/view.php?id=27000"
     ["Classroom Fisica"]="https://classroom.google.com/c/ODc0MzgwMTMwMTMy"
-    ["Resumen history of engineering"]="https://docs.google.com/document/d/13b3zTCxSoct6I5jkDZvJbPcNSg35V_hyLQMKzU7uBBU/edit?usp=sharing"
+    ["Parcial alge.lineal"]="https://virtualingenieriaudea.co/mod/quiz/view.php?id=874672"
+    ["Podcast ingles"]="https://docs.google.com/document/d/1SabO7v-g6kPV2kS-aSma3Y0SgAYu1qn5-Lh5WOsYnR8/edit?usp=sharing"
     ["Drive Ingles"]="https://udeaeduco-my.sharepoint.com/shared?listurl=https%3A%2F%2Fudeaeduco%2Dmy%2Esharepoint%2Ecom%2Fpersonal%2Faracelly%5Fospina%5Fudea%5Fedu%5Fco%2FDocuments&e=5%3Abb3aac9a086840b9859382a8c185882f&sharingv2=true&fromShare=true&at=9&id=%2Fpersonal%2Faracelly%5Fospina%5Fudea%5Fedu%5Fco%2FDocuments%2F2026%2D2%20EforE%20students%2FEnglish%20V%2FGroup%2018%20%2D%202026%2D2&FolderCTID=0x012000A5F08FC96EEC3B468C71B876883C6992"
 
     ["All subjets UdeA"]="all.sh"
